498d96b3a9043dfdd4dcff1ce9cc29f33da9ab73
Nastya <nastya_mi@icloud.com>
2019-12-03 23:42:32 +0300

    feat: add projects links to slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const panel = document.getElementsByClassName('expansion-panel');
+const sliderItems = document.querySelectorAll('.projects__slide');
+
+let currentItem = 0;
+let isEnabled = true;
 
 for (let i = 0; i < panel.length; i++) {
   panel[i].addEventListener('click', function () {
@@ -11,3 +15,48 @@ for (let i = 0; i < panel.length; i++) {
     }
   });
 }
+
+function hideItem(direction) {
+  isEnabled = false;
+  sliderItems[currentItem].classList.add(direction);
+  sliderItems[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('active', direction);
+  });
+}
+
+function showItem(direction) {
+  sliderItems[currentItem].classList.add('next', direction);
+  sliderItems[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('next', direction);
+    this.classList.add('active');
+    isEnabled = true;
+  });
+}
+
+function changeSliderItem(n) {
+  currentItem = (n + sliderItems.length) % sliderItems.length;
+}
+
+function previousSliderItem(n) {
+  hideItem('toright');
+  changeSliderItem(n - 1);
+  showItem('fromleft');
+}
+
+function nextSliderItem(n) {
+  hideItem('toleft');
+  changeSliderItem(n + 1);
+  showItem('fromright');
+}
+
+document.querySelector('.projects__slider-controls.left').addEventListener('click', function () {
+  if (isEnabled) {
+    previousSliderItem(currentItem);
+  }
+});
+
+document.querySelector('.projects__slider-controls.right').addEventListener('click', function () {
+  if (isEnabled) {
+    nextSliderItem(currentItem);
+  }
+});
