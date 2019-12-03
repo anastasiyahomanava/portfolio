1543f32842cbd0db0ddcefb4f057c68e49177d70
Nastya <nastya_mi@icloud.com>
2019-12-03 17:33:30 +0300

    refactor: refactor project structure

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Portfolio
 
-## About
-Here is my portfolio task
+- Task - <https://github.com/rolling-scopes-school/tasks/blob/master/tasks/stage-1/portfolio/portfolio-ru.md>
+
+## How to start
+
+To look my project you need to clone my repository:
+
+```sh
+git clone https://github.com/anastasiyahomanava/portfolio.git
+```
---
+++ js/main.js
@@ -0,0 +1,13 @@
+const panel = document.getElementsByClassName('expansion-panel');
+
+for (let i = 0; i < panel.length; i++) {
+  panel[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    let content = this.nextElementSibling;
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+  });
+}
